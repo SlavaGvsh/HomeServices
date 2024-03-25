f4677dd51809e8c47b6d51dbe7dcab313225389b
Slava.Gvsh <slava.gvsh@gmail.com>
2024-03-25 18:08:43 +0200

    header

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
     $('.header__burger').click(function(event) {
-       $('.header__burger,.header__menu').toggleClass('active');
-       $('body').toggleClass('lock');
+       $('.header__body,.mobile-header, .header__container').addClass('active');
+       $('body').addClass('lock');
  
     });
+    $('.header__close').click(function(event) {
+      $('.header__body, .header__container').removeClass('active');
+      $('body').removeClass('lock');
+
+   });
+
  });
  
